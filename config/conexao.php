5f268dd03893ebe7cd79e657a412f15006a5187a
Joao Felipe <joaofelipeaires123@gmail.com>
2026-04-22 19:33:48 -0300

    fix php extensions

--- config/conexao.php
+++ config/conexao.php
@@ -2,7 +2,7 @@
 define ("DB_HOST", getenv('DB_HOST'));
 define ("DB_NAME", getenv('DB_NAME'));
 define ("DB_USER", getenv('DB_USER'));
-define ("DB_PASS", getenv('DB_PASS'));
+define ("DB_PASS", getenv('DB_PASSWORD'));
 
 try {
     $dns = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8mb4";
